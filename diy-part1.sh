6ad218e5a9dc7e4eb30c62558a84c75163eee740
wekingchen <weking.chen@gmail.com>
2023-11-25 11:54:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 
 # Add a feed source
-echo "src-git helloworld https://github.com/fw876/helloworld.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git;main" >> "feeds.conf.default"
 
 ./scripts/feeds clean
