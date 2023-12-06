fa14e7863ba8c833f5f13dcdab11e5a9f13d696a
wekingchen <weking.chen@gmail.com>
2023-12-06 11:23:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,20 +13,17 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=118.0.5993.65-1/' feeds/PWpackages/naiveproxy/Makefile
-#sed -i 's/^PKG_HASH:=.*/PKG_HASH:=d97193d82e346ec2f753151cee52d00495e2df7688d60a5b4395b8cd39399734/' feeds/PWpackages/naiveproxy/Makefile
-#sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=118.0.5993.65-1/' feeds/helloworld/naiveproxy/Makefile
-#sed -i 's/^PKG_HASH:=.*/PKG_HASH:=d97193d82e346ec2f753151cee52d00495e2df7688d60a5b4395b8cd39399734/' feeds/helloworld/naiveproxy/Makefile
-
 # 替换luci-theme-argon相关
 # rm -rf feeds/luci/themes/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
 # 添加aliyundrive-webdav
 rm -rf feeds/packages/multimedia/aliyundrive-webdav
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav feeds/packages/multimedia/aliyundrive-webdav
 rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications/luci-app-aliyundrive-webdav
+git clone https://github.com/messense/aliyundrive-webdav.git
+cp -r aliyundrive-webdav/openwrt/aliyundrive-webdav feeds/packages/multimedia
+cp -r aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav feeds/luci/applications
+rm -rf aliyundrive-webdav
 
 # 跟随最新版naiveproxy
 rm -rf feeds/passwall_packages/naiveproxy
