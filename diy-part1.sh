5c3e28e320f4b33bcb4e94903087d0f79db533c6
wekingchen <weking.chen@gmail.com>
2026-09-17 14:26:35 +0800

    Comment out helloworld feed source addition
    Comment out the addition of the helloworld feed source in feeds.conf.default.

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,6 @@
 # Add a feed source
 echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
+#echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
 
 ./scripts/feeds clean
