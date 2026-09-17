efd4aaad6e3ed7283cb70c67ce9c152e9f8ffe47
wekingchen <weking.chen@gmail.com>
2026-09-17 14:44:38 +0800

    Update Go source for xray version and frp version

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,9 +29,9 @@ cp -r feeds/passwall_packages/dns2socks feeds/packages/net
 # cp -r feeds/passwall_packages/dns2tcp feeds/packages/net
 cp -r feeds/passwall_packages/microsocks feeds/packages/net
 
-# 修改golang源码以编译xray1.8.8+版本
+# 修改golang源码以编译xray26.9.9+版本
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 27.x feeds/packages/lang/golang
 sed -i '/-linkmode external \\/d' feeds/packages/lang/golang/golang-package.mk
 
 # 修改frp版本为官网最新v0.68.1 https://github.com/fatedier/frp 格式：https://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
